3c729b59911e5a2f94d4c5d560c82bf55256f968
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2024-06-18 15:26:44 -0400

    work

--- app_e_commerce/models.py
+++ app_e_commerce/models.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, Integer, String
 from app_e_commerce.database import Base
 
 class CustomerOrder(Base):
-    __tablename__ = "Orders"
+    __tablename__ = "Orders_New"
 
     id = Column(Integer, primary_key=True)
     email = Column(String(50))
     address = Column(String(100))
     order = Column(String(250))
-    total = (Integer)
+    total = Column(Integer)
